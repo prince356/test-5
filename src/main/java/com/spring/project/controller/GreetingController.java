931eb5355b3e3763956f0767442763d9a39968a6
prince356 <princechaurasia014@gmail.com>
2025-03-10 17:32:26 +0530

    Added: Return JSON for different HTTP methods

---
+++ src/main/java/com/spring/project/controller/GreetingController.java
@@ -0,0 +1,35 @@
+package com.spring.project.controller;
+
+import com.spring.project.dto.GreetingDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class GreetingController {
+
+    @GetMapping
+    public GreetingDTO getGreeting() {
+        return new GreetingDTO("Hello, World!");
+    }
+
+    @PostMapping
+    public GreetingDTO createGreeting(@RequestBody GreetingDTO greeting) {
+        return new GreetingDTO("Created: " + greeting.getMessage());
+    }
+
+    @PutMapping
+    public GreetingDTO updateGreeting(@RequestBody GreetingDTO greeting) {
+        return new GreetingDTO("Updated: " + greeting.getMessage());
+    }
+
+    @DeleteMapping
+    public GreetingDTO deleteGreeting() {
+        return new GreetingDTO("Deleted");
+    }
+}
---
+++ src/main/java/com/spring/project/dto/GreetingDTO.java
@@ -0,0 +1,12 @@
+package com.spring.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GreetingDTO {
+    private String message;
+}
